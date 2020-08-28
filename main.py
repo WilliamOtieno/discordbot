605fd2b81f7257a52c62dfa330f386eb1104e613
WilliamOtieno <jimmywilliamotieno@gmail.com>
2020-08-28 12:02:28 +0300

    Added kick and ban

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from discord.ext import commands
-
 import secrets
+import random
+import discord
 
-bot = commands.Bot(command_prefix=".")
+bot = commands.Bot(command_prefix="/")
 
 
 @bot.event
@@ -18,6 +19,39 @@ async def on_member_join(member):
 @bot.event
 async def on_member_remove(member):
     print(f'{member} has left the server.')
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f"Pong! {round(bot.latency * 1000)} ms")
+
+
+@bot.command(aliases=["8ball", "ask"])
+async def _8ball(ctx, *, question):
+    responses = [
+        "It is certain",
+        "I'm not sure",
+        "Dont count on it",
+        "My reply is no",
+        "My sources say no"
+    ]
+    await ctx.send(f"Question: {question}\nAnswer: {random.choice(responses)}")
+
+
+@bot.command()
+async def clear(ctx, amount=5):
+    await ctx.channel.purge(limit=amount)
+    print(f"Cleared {amount} messages")
+
+
+@bot.command()
+async def kick(ctx, member: discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+
+
+@bot.command()
+async def ban(ctx, member: discord.Member, *, reason=None):
+    await member.ban(reason=reason)
 
 
 bot.run(secrets.token)
